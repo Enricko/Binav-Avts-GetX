7815cf68ee2203fc2693d7160119c31e4b6f7fb7
fadillarizky <fadillarizky294@gmail.com>
2024-03-18 15:48:04 +0700

    update UI and add current location

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,7 @@ import 'dart:html' as html;
 import 'package:binav_avts_getx/controller/map.dart';
 import 'package:binav_avts_getx/model/get_kapal_coor.dart';
 import 'package:binav_avts_getx/pages/table/first_profile.dart';
+import 'package:binav_avts_getx/utils/alerts.dart';
 import 'package:binav_avts_getx/widget/maps/ruler/marker_cursor_follow.dart';
 import 'package:binav_avts_getx/widget/maps/ruler/ruler_line.dart';
 import 'package:binav_avts_getx/widget/maps/vessel/vessel.dart';
@@ -11,6 +12,7 @@ import 'package:binav_avts_getx/widget/maps/vessel/vessel_line_latlong.dart';
 import 'package:binav_avts_getx/widget/maps/vessel/window_vessel_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:latlong2/latlong.dart';
@@ -52,22 +54,22 @@ class HomePage extends StatelessWidget {
                       onMapEvent: (event) {
                         mapGetController.updatePoint(null);
                       },
-                      onTap: (mapGetController.countDistance.value)
-                          ? (TapPosition, LatLong) {
-                              mapGetController.handleMapTap(LatLong);
-                            }
-                          : null,
-                      onPointerHover: (PointerHoverEvent, LatLng) {
-                        if (mapGetController.countDistance.value) {
-                          mapGetController.latLngCursor.value = LatLng;
-                        }
-                      },
-                      onSecondaryTap: (TapPosition, LatLong) {
-                        // mapGetController.maxLengthMarker.value --;
-                        mapGetController.countDistance.value = false;
-                        // mapGetController.markers.clear();
-                        // mapGetController.markersLatLng.clear();
-                      },
+                      // onTap: (mapGetController.countDistance.value)
+                      //     ? (TapPosition, LatLong) {
+                      //         mapGetController.handleMapTap(LatLong);
+                      //       }
+                      //     : null,
+                      // onPointerHover: (PointerHoverEvent, LatLng) {
+                      //   if (mapGetController.countDistance.value) {
+                      //     mapGetController.latLngCursor.value = LatLng;
+                      //   }
+                      // },
+                      // onSecondaryTap: (TapPosition, LatLong) {
+                      // mapGetController.maxLengthMarker.value --;
+                      // mapGetController.countDistance.value = false;
+                      // mapGetController.markers.clear();
+                      // mapGetController.markersLatLng.clear();
+                      // },
                       minZoom: 4,
                       maxZoom: 18,
                       initialZoom: mapGetController.initialZoom.value,
@@ -75,21 +77,22 @@ class HomePage extends StatelessWidget {
                       onPositionChanged: (position, hasGesture) {
                         mapGetController.setUserCurrentPosition(
                             position.zoom!, position.center!);
+                        mapGetController.latLngCursor.value =
+                            mapGetController.mapController.center;
                       },
                     ),
                     nonRotatedChildren: [
-                      /// button zoom in/out kanan bawah
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: const FlutterMapZoomButtons(
-                          minZoom: 4,
-                          maxZoom: 18,
-                          mini: true,
-                          padding: 10,
-                          zoomInColorIcon: Colors.black,
-                          zoomOutColorIcon: Colors.black,
-                        ),
-                      ),
+                      // Align(
+                      //   alignment: Alignment.bottomRight,
+                      //   child: const FlutterMapZoomButtons(
+                      //     minZoom: 4,
+                      //     maxZoom: 18,
+                      //     mini: true,
+                      //     padding: 10,
+                      //     zoomInColorIcon: Colors.black,
+                      //     zoomOutColorIcon: Colors.black,
+                      //   ),
+                      // ),
                       Column(
                         children: [
                           Container(
@@ -121,11 +124,14 @@ class HomePage extends StatelessWidget {
                         }
                         return SizedBox();
                       }),
-                      
-                      // Align(
-                      //   alignment: Alignment.center,
-                      //   child: Icon(Icons.location_pin),
-                      // ),
+
+                      (mapGetController.countDistance.value)
+                          ? Align(
+                              alignment: Alignment.center,
+                              child: Icon(Icons.location_searching_outlined,
+                                  color: Colors.white, size: 15),
+                            )
+                          : SizedBox(),
 
                       Obx(
                         () {
@@ -217,27 +223,27 @@ class HomePage extends StatelessWidget {
                                           MaterialStateProperty.all(
                                               Colors.white)),
                                   onPressed: () {
+                                    print(
+                                        mapGetController.mapController.center);
+                                    mapGetController.isBottomBarVisible.value =
+                                        !mapGetController
+                                            .isBottomBarVisible.value;
                                     mapGetController.countDistance.value =
-                                    !mapGetController.countDistance.value;
+                                        !mapGetController.countDistance.value;
                                     if (mapGetController.countDistance.value ==
                                         false) {
                                       mapGetController.markers.clear();
                                       mapGetController.markersLatLng.clear();
-                                      html
-                                          .window.onContextMenu
-                                          .listen((event) {
-                                        event.preventDefault();
-                                      }).cancel();
-                                    }else{
-                                      html
-                                          .window.onContextMenu
-                                          .listen((event) {
-                                        event.preventDefault();
-                                      });
-                                      mapGetController.latLngCursor.value = null;
+                                      // html.window.onContextMenu.listen((event) {
+                                      //   event.preventDefault();
+                                      // }).cancel();
+                                    } else {
+                                      // html.window.onContextMenu.listen((event) {
+                                      //   event.preventDefault();
+                                      // });
+                                      mapGetController.latLngCursor.value =
+                                          null;
                                     }
-
-
                                   },
                                   icon: Icon(
                                     Icons.straighten,
@@ -248,9 +254,162 @@ class HomePage extends StatelessWidget {
                                 ),
                               ),
                             ),
+                            SizedBox(
+                              width: 10,
+                            ),
                           ],
                         ),
                       ),
+
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Tooltip(
+                              message: "Location",
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                    left: 10, top: 10, right: 10),
+                                child: FloatingActionButton(
+                                  mini: true,
+                                  backgroundColor: Colors.white,
+                                  onPressed: () async {
+                                    ///check apakah button get current location active
+                                    if (mapGetController
+                                            .currentPositionActive.value ==
+                                        false) {
+                                      /// check permission
+                                      mapGetController.permission =
+                                          await Geolocator.checkPermission();
+                                      if (mapGetController.permission ==
+                                          LocationPermission.denied) {
+                                        mapGetController.permission =
+                                            await Geolocator
+                                                .requestPermission();
+                                        if (mapGetController.permission ==
+                                            LocationPermission.denied) {
+                                          return Future.error(
+                                              'Location permissions are denied');
+                                        }
+                                      }
+
+                                      if (mapGetController.permission ==
+                                          LocationPermission.deniedForever) {
+                                        return Future.error(
+                                            'Location permissions are permanently denied, we cannot request permissions.');
+                                      }
+                                      /// when getting the lattng,button currentposition is active
+                                      Position position =
+                                          await Geolocator.getCurrentPosition(
+                                              desiredAccuracy:
+                                                  LocationAccuracy.high).whenComplete(() {
+                                                    mapGetController.currentPositionActive.value = true;
+                                          });
+                                      mapGetController.currentPosition.value =
+                                          LatLng(position.latitude,
+                                              position.longitude);
+                                      mapGetController.mapController.move(
+                                          LatLng(position.latitude,
+                                              position.longitude),
+                                          15);
+                                    }
+                                    else{
+                                      mapGetController.currentPositionActive.value = false;
+                                      mapGetController.currentPosition.value = null;
+                                    }
+                                  },
+                                  child: Icon(Icons.location_searching_outlined,
+                                      color: mapGetController
+                                              .currentPositionActive.value
+                                          ? Colors.blue
+                                          : Colors.black),
+                                ),
+                              ),
+                            ),
+                            FlutterMapZoomButtons(
+                              minZoom: 4,
+                              maxZoom: 18,
+                              mini: true,
+                              padding: 10,
+                              zoomInColorIcon: Colors.black,
+                              zoomOutColorIcon: Colors.black,
+                            ),
+                            AnimatedContainer(
+                              duration: Duration(milliseconds: 500),
+                              // Sesuaikan durasi animasi sesuai kebutuhan
+                              height: mapGetController.isBottomBarVisible.value
+                                  ? kBottomNavigationBarHeight
+                                  : 0,
+                              child: Container(
+                                color: Colors.white,
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 20),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        "Klik Icon (+) untuk mengukur jarak",
+                                        style: TextStyle(color: Colors.black54),
+                                      ),
+                                      Row(
+                                        children: [
+                                          IconButton(
+                                              style: ButtonStyle(
+                                                  backgroundColor:
+                                                      MaterialStateProperty.all(
+                                                          Colors.white)),
+                                              onPressed: () {
+                                                mapGetController.markers
+                                                    .removeLast();
+                                                mapGetController.markersLatLng
+                                                    .removeLast();
+// mapGetController.latLngCursor.removeLast();
+                                              },
+                                              icon: Icon(Icons.undo)),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          IconButton(
+                                              style: ButtonStyle(
+                                                backgroundColor:
+                                                    MaterialStateProperty.all(
+                                                        Colors.blue),
+                                              ),
+                                              onPressed: (mapGetController
+                                                      .countDistance.value)
+                                                  ? () {
+                                                      mapGetController
+                                                              .latLngCursor
+                                                              .value =
+                                                          mapGetController
+                                                              .mapController
+                                                              .center;
+                                                      mapGetController
+                                                          .handleMapTap(
+                                                              mapGetController
+                                                                  .mapController
+                                                                  .center);
+                                                    }
+                                                  : null,
+                                              icon: Icon(
+                                                Icons.add,
+                                                color: Colors.white,
+                                              )),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                // height: 80,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+
                       // Align(
                       //   alignment: Alignment.centerLeft,
                       //   child: Padding(
@@ -297,14 +456,31 @@ class HomePage extends StatelessWidget {
                       VesselWidget(),
                       // Garis kapal
                       // VesselLineLatlong(),
+                      RulerLine(),
+
                       Obx(() {
                         return MarkerLayer(
                             markers: mapGetController.markers.value);
                       }),
+                      mapGetController.currentPosition.value != null
+                          ? MarkerLayer(
+                              markers: [
+                                Marker(
+                                    width: 10,
+                                    point:
+                                        mapGetController.currentPosition.value!,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.blue,
+                                          shape: BoxShape.circle,
+                                          border:
+                                              Border.all(color: Colors.white)),
+                                    )),
+                              ],
+                            )
+                          : SizedBox()
 
-                      RulerLine(),
-
-                      MarkerCursorFollow(),
+                      // MarkerCursorFollow(),
 
                       // MarkerLayer(
                       //   markers: [
